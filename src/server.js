30aecc9cf8753d92207a20043e0431e3919f80bc
andres-olarte396 <andres.olarte396@gmail.com>
2025-01-03 00:22:02 -0500

    :sparkles: Actualizar el servidor para que escuche en el puerto especificado y mejorar el mensaje de inicio

--- src/server.js
+++ src/server.js
@@ -17,13 +17,15 @@ const corsOptions = {
   methods: 'POST',
   allowedHeaders: 'Content-Type,Authorization',
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
+
 app.use('/api', secretRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 const PORT = process.env.PORT || 3501;
-tlsMiddleware.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor de secretos corriendo en el puerto ${PORT}`);
   console.log(`Documentación disponible en https://localhost:${PORT}/api-docs`);
 });
